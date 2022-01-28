fc47217f5b8616caffa0eb7a0db6adefab9a1e24
Timothy Coleman <timothy.coleman@gmail.com>
2022-01-28 18:07:47 +0000

    careful reasoning and testing the cases of the collision detector

---
+++ src/EventStore.Core.XUnit.Tests/Scavenge/CollisionDetectorTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventStore.Core.Index.Hashes;
+using EventStore.Core.TransactionLog.Scavenging;
+using Xunit;
+
+namespace EventStore.Core.XUnit.Tests.Scavenge {
+	class MockHahser : ILongHasher<string> {
+		public ulong Hash(string x) =>
+			(ulong)(x.Length == 0 ? 0 : x[0].GetHashCode());
+	}
+
+	public class CollisionDetectorTests {
+		public static IEnumerable<object[]> TheCases() {
+			var none = Array.Empty<string>();
+			
+			// the first letter of the stream name determines the the hash
+			// each row is a record but we don't care much about it, just its stream name.
+			yield return Case(
+				"seen stream before. was a collision first time we saw it",
+				("a-stream1", none),
+				("a-streamOfInterest", new[] { "a-stream1", "a-streamOfInterest" }),
+				("b-stream2", none),
+				("a-streamOfInterest", none));
+
+			yield return Case(
+				"seen stream before. was not a collision but has since been collided with",
+				("a-streamOfInterest", none),
+				("a-stream1", new[] { "a-stream1", "a-streamOfInterest" }),
+				("b-stream2", none),
+				("a-streamOfInterest", none));
+
+			yield return Case(
+				"seen stream before. was not a collision and still isnt",
+				("a-streamOfInterest", none),
+				("b-stream2", none),
+				("a-streamOfInterest", none));
+
+			yield return Case(
+				"first time seeing stream. no collision",
+				("a-stream1", none),
+				("b-stream2", none),
+				("c-streamOfInterest", none));
+
+			yield return Case(
+				"first time seeing stream. collides with previous stream",
+				("a-stream1", none),
+				("b-stream2", none),
+				("a-streamOfInterest", new[] { "a-stream1", "a-streamOfInterest" }));
+
+			yield return Case(
+				"three way collision",
+				("a-stream1", none),
+				("a-stream2", new[] { "a-stream1", "a-stream2" }),
+				("a-stream3", new[] { "a-stream3" }));
+
+			yield return Case(
+				"in combination",
+				("a-stream1", none), // 2b
+				("a-stream2", new[] { "a-stream1", "a-stream2" }), // 2a
+				("b-stream3", none),
+				("a-stream4", new[] { "a-stream4" }),
+				("b-stream3", none), // 1c
+				("a-stream1", none), // 1b
+				("a-stream2", none)); // 1a
+
+			static object[] Case(string name, params (string, string[])[] data) {
+				return new object[] {
+					name, data
+				};
+			}
+		}
+
+		[Theory]
+		[MemberData(nameof(TheCases))]
+		public void Works(string caseName, (string StreamName, string[] NewCollisions)[] data) {
+			Assert.NotNull(caseName);
+
+			var hasher = new MockHahser();
+
+			var index = new Dictionary<ulong, List<int>>();
+			for (var i = 0; i < data.Length; i++) {
+				var streamName = data[i].StreamName;
+				var hash = hasher.Hash(streamName);
+				if (!index.TryGetValue(hash, out var entries)) {
+					entries = new();
+					index[hash] = entries;
+				}
+				entries.Add(i);
+			}
+
+			// maps hashes to stream names
+			var cache = new Dictionary<ulong, string>();
+
+			// simulates looking in the index for records with the current hash up to
+			// the all stream positionLimit. exclude the positionLimit itself because that is the
+			// position of the record that the hash came from.
+			//qq the index version of this wont be that expensive, but wont be that cheap either
+			// and we'll be calling it for every record. so we could definitely do with a cache
+			// as long as that doesn't break any of the properties that we require
+			bool HashInUse(string streamName, long positionLimit, out string candidateCollidee) {
+				var hash = hasher.Hash(streamName);
+
+				if (cache.TryGetValue(hash, out candidateCollidee))
+					return true;
+
+				// simulate hitting the ptables. if we find any entries for this hash
+				// then look up the record for one of them to get the stream name
+				if (index.TryGetValue(hash, out var entries)) {
+					foreach (var entry in entries) {
+						if (entry < positionLimit) {
+							var candidate = data[entry].StreamName;
+							candidateCollidee = candidate;
+							cache[hash] = candidate;
+							return true;
+						}
+					}
+				}
+
+				cache[hash] = streamName;
+				candidateCollidee = default;
+				return false;
+			}
+
+			var sut = new CollisionDetector<string>(HashInUse);
+
+
+			var expectedCollisions = new HashSet<string>();
+
+			for (var i = 0; i < data.Length; i++) {
+				foreach (var newCollision in data[i].NewCollisions)
+					expectedCollisions.Add(newCollision);
+
+				sut.Add(data[i].StreamName, i);
+				Assert.Equal(
+					expectedCollisions.OrderBy(x => x),
+					sut.GetAllCollisions());
+			}
+		}
+	}
+}
---
+++ src/EventStore.Core/TransactionLog/Scavenging/CollisionDetector.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EventStore.Core.TransactionLog.Scavenging {
+	// add things to the collision detector and it keeps a list of things that collided.
+	public class CollisionDetector<T> where T : IEquatable<T> {
+		// checks if the hash is in use before the position. returns true if so.
+		// if returning true then out parameter is one of the items that hashes to that hash
+		public delegate bool HashInUse(T item, long positionLimit, out T candidateCollidee);
+
+		private static EqualityComparer<T> TComparer { get; } = EqualityComparer<T>.Default;
+		private readonly HashInUse _hashInUse;
+
+		// store the values. could possibly store just the hashes instead but that would
+		// lose us information and it should be so rare that there are any collisions at all.
+		//qq for the real implementation make sure adding is idempotent
+		private readonly HashSet<T> _collisions;
+		
+		//qq will need something like this to tell where to continue from. maybe not in this class though
+		private long _lastPosition;
+
+		public CollisionDetector(HashInUse hashInUse) {
+			_collisions = new();
+			_hashInUse = hashInUse;
+		}
+
+		public bool IsCollision(T item) => _collisions.Contains(item);
+		public T[] GetAllCollisions() => _collisions.OrderBy(x => x).ToArray();
+
+		// allllright. when we are adding an item, either:
+		//   1. we have seen this stream before.
+		//       then either:
+		//       a. it was a collision the first time we saw it
+		//           - by induction we concluded it was a collision at that time and added it to the
+		//             collision list so we conclude it is a collision now
+		//             TICK
+		//       b. it was not a collision but has since been collided with by a newer stream.
+		//           - when it was collided with we noticed and added it to the collision list so
+		//             we conclude it is still a collision now
+		//             NB: it is important that we added this stream to the collision list. consider:
+		//                 if we look in the index to see if there are any entries for this hash
+		//                 there would be, because this is the collision case
+		//                 and, crucially, those entries would be a mix of this stream and other streams
+		//                 because this is the 'seen before' case.
+		//                 so we couldn't tell by looking up a single entry whether this was a collision
+		//                 or not. BAD.
+		//       c. it was not a collision and still isn't
+		//           - so we look in the index to see if there are any entries for this hash
+		//             there are, because we have seen this stream before
+		//             all the entries are for this stream, but we only want to check one.
+		//             so we check one, and we find that it is for this stream
+		//             conclude no collision
+		//             TICK
+		//
+		//   2. this is the first time we are seeing this stream.
+		//       then either:
+		//       a. it collides with any stream we have seen before
+		//           i. it collides with 1 stream that hasnt been in a collision up til now
+		//           ii. it collides with 2+ streams that are already colliding with each other.
+		//           both of these cases are handled in the same way below. but if we stored the hash we
+		//           could handle (ii) differently but super rare so dont bother
+		//           - so we look in the index to see if there are any entries for this hash
+		//             there are, because this is the colliding case.
+		//             we pick any entry and look up the record
+		//             the record MUST be for a different stream because this is the the first time
+		//                we are seeing this stream.
+		//             collision detected.
+		//             add both streams to the collision list. we have their names and the hash.
+		//             BUT is it possible that we are colliding with multiple streams, do we need to add
+		//                    them all to the hash? it is possible yes, but they are already in the hash
+		//                    because they are colliding with each other.
+		//             TICK
+		//       b. it does not collide with any stream we have seen before
+		//           - so we look in the index to see if there are any entries for this hash
+		//             there are not because this is the first time we are seeing this stream and it does
+		//                not collde.
+		//             conclude no collision
+		//             TICK
+		public void Add(T item, long position) {
+			_lastPosition = position;
+
+			if (IsCollision(item)) {
+				return; // previously known collision.
+			}
+
+			// collision not previously known, but might be a new one now.
+			if (!_hashInUse(item, position, out var candidateCollidee)) {
+				return; // hash not in use, can be no collision.
+			}
+
+			// hash in use, but maybe by the item itself.
+			if (TComparer.Equals(candidateCollidee, item)) {
+				return; // no collision with oneself
+			}
+
+			// hash in use by a different item! found new collision.
+			_collisions.Add(item);
+			_collisions.Add(candidateCollidee);
+		}
+	}
+}
